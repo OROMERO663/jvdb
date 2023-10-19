0dec68c57d4f2d8c54a9de3b85955b99aa5710ed
OROMERO663 <oromeroe663@alumnos.imf.com>
2023-10-19 23:17:34 +0200

    Atrapar operaciones con If y Else if

--- jvdb.c
+++ jvdb.c
@@ -3,19 +3,28 @@
 
 int main(int argc, char *argv[]){
     FILE *archivo;
-    char *basededatos = argv[1];
-    char *nombreArchivo = argv[2];
-    
-    //strcpy copia el string, juntandolo podemos crear un string por trozos
-    char ruta[100];
-    strcpy(ruta,basededatos);
-    strcat(ruta,"-");
-    strcat(ruta,nombreArchivo);
-    strcat(ruta,".txt");
-    
-    archivo = fopen(ruta,"a");
-    char *texto = argv[3];
-    fputs(strcat(texto,"\n"),archivo);
-    fclose(archivo);
+    char *operacion = argv[1];
+    //strcmp es String Compare, compara 2 strings
+    if(strcmp(operacion,"select")==0){
+        printf("Te doy datos");
+    }else if(strcmp(operacion,"insert")==0){
+        char *basededatos = argv[2];
+        char *nombreArchivo = argv[3];
+
+        char ruta[100];
+        strcpy(ruta,basededatos);
+        strcat(ruta,"-");
+        strcat(ruta,nombreArchivo);
+        strcat(ruta,".txt");
+
+        archivo = fopen(ruta,"a");
+        char *texto = argv[4];
+
+        fputs(strcat(texto,"\n"),archivo);
+        fclose(archivo);
+        
+    }else {
+        printf("La operación no es correcta");
+    }
     return 0;
 }
