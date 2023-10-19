061bdf5d35dba207fc465aadb6c659d1c9daf98a
OROMERO663 <oromeroe663@alumnos.imf.com>
2023-10-19 23:07:22 +0200

    Especificar Base de datos

--- jvdb.c
+++ jvdb.c
@@ -3,9 +3,18 @@
 
 int main(int argc, char *argv[]){
     FILE *archivo;
-    char *nombreArchivo = argv[1];
-    archivo = fopen(strcat(nombreArchivo,".txt"),"a");
-    char *texto = argv[2];
+    char *basededatos = argv[1];
+    char *nombreArchivo = argv[2];
+    
+    //strcpy copia el string, juntandolo podemos crear un string por trozos
+    char ruta[100];
+    strcpy(ruta,basededatos);
+    strcat(ruta,"-");
+    strcat(ruta,nombreArchivo);
+    strcat(ruta,".txt");
+    
+    archivo = fopen(ruta,"a");
+    char *texto = argv[3];
     fputs(strcat(texto,"\n"),archivo);
     fclose(archivo);
     return 0;
