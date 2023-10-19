082522b5c2fc64525b9ef59467b64ca7e581c1fb
OROMERO663 <oromeroe663@alumnos.imf.com>
2023-10-19 23:32:44 +0200

    Insert y Select completos

--- jvdb.c
+++ jvdb.c
@@ -4,22 +4,29 @@
 int main(int argc, char *argv[]){
     FILE *archivo;
     char *operacion = argv[1];
-    //strcmp es String Compare, compara 2 strings
+    char *basededatos = argv[2];
+    char *nombreArchivo = argv[3];
+    
+    char ruta[100];
+    strcpy(ruta,basededatos);
+    strcat(ruta,"-");
+    strcat(ruta,nombreArchivo);
+    strcat(ruta,".txt");
+    
+    
     if(strcmp(operacion,"select")==0){
-        printf("Te doy datos");
+        archivo = fopen(ruta,"r");
+        printf("Te doy datos:\n");
+        char linea[1024];
+        //Vamos a atrapar dentro de la línea, el tamaño de la linea con respecto al archivo abierto
+        //Mientras que lo de dentro no sea nulo va a funcionar
+        while(fgets(linea,sizeof(linea),archivo) != NULL){
+            printf("Linea: %s", linea);
+        }
+            
     }else if(strcmp(operacion,"insert")==0){
-        char *basededatos = argv[2];
-        char *nombreArchivo = argv[3];
-
-        char ruta[100];
-        strcpy(ruta,basededatos);
-        strcat(ruta,"-");
-        strcat(ruta,nombreArchivo);
-        strcat(ruta,".txt");
-
         archivo = fopen(ruta,"a");
         char *texto = argv[4];
-
         fputs(strcat(texto,"\n"),archivo);
         fclose(archivo);
         
